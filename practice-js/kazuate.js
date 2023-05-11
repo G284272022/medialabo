4c5243a74e69e12b5d36171d106170c4cd4c1d80
G284272022 <105624123+G284272022@users.noreply.github.com>
2023-05-11 16:18:31 +0900

    変更

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -4,7 +4,6 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -17,7 +16,9 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  for(let n of kaisu){
   kaisu+=1;
+  console.log(kaisu+'回目の予想:' +yoso);
   if (kaisu > 4) {
     console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
   }
@@ -34,6 +35,7 @@ function hantei() {
         console.log("正解です.おめでとう!");
       }
   }
+}
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
