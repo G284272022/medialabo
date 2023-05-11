ab23940c8ad7e9ec21e5e8b93f80262317ba5fcf
G284272022 <105624123+G284272022@users.noreply.github.com>
2023-05-11 16:38:58 +0900

    変更

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -18,7 +18,7 @@ function hantei() {
   let yoso = 4;
   kaisu+=1;
   console.log(kaisu+'回目の予想:' +yoso);
-  if (kaisu > 4) {
+  if (kaisu >= 4) {
     console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
   }else if(kaisu==3){
     console.log("まちがい.残念でした答えは" + kotae +"です．"); 
