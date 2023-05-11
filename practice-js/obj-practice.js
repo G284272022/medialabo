e1e8ee0b5aacfd4c9663c2e8264ad7c66b9bbebb
G284272022 <105624123+G284272022@users.noreply.github.com>
2023-05-11 14:40:58 +0900

    for追加

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,10 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+for (let i=0; i<campus.length; i=i+1) {
+    console.log(campus[i]);
+}
+for (let i=0; i<gakka.length; i=i+1) {
+    console.log(gakka[i]);
+}
 
