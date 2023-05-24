0f2a139fd8496b341a691d375d6c546841eb02cb
G284272022 <105624123+G284272022@users.noreply.github.com>
2023-05-24 18:00:28 +0900

    もろもろ追加

--- weather.js
+++ weather.js
@@ -46,6 +46,9 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-console.log("都市名: "+data.name);  
-console.log("最高気温: "+data.main.temp_max);
-console.log("最低気温: "+data.main.temp_min);
+let p1 = document.querySelector('p#name1');
+p1.textContent = ("都市名: "+data.name); 
+let p2 = document.querySelector('p#temp_max1');
+p2.textContent = ("最高気温: "+data.main.temp_max);
+let p3 = document.querySelector('p#temp_min1');
+p3.textContent = ("最低気温: "+data.main.temp_min);
